1b65bf6b3dc69469183a4aa22ed621292bcb81c8
alok-hazarika <dev.alok2018@gmail.com>
2020-08-30 02:49:52 +0530

    Added Input fields

--- exptrc/lib/main.dart
+++ exptrc/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +18,32 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 'T1',
+      tittle: 'New veg purchase',
+      amount: 1720.61,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'T2',
+      tittle: 'New fruit purchase',
+      amount: 1760.98,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'T3',
+      tittle: 'New exotic purchase',
+      amount: 7786.99,
+      date: DateTime.now(),
+    ),
+  ];
+  // String tittleInput;
+  // String amountInput;
+
+  final tittleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +51,7 @@ class MyHomePage extends StatelessWidget {
           title: Text('Expense Tracker'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
@@ -34,8 +63,78 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
             Card(
-              color: Colors.red,
-              child: Text('List expenses'),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: tittleController,
+                      // onChanged: (value) {
+                      //   tittleInput = value;
+                      // },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      controller: amountController,
+                      // onChanged: (value) => amountInput = value,
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        print(tittleController.text);
+                        print(amountController.text);
+                      },
+                      child: Text('Add Expense'),
+                      textColor: Colors.blue[300],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'INR ${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.tittle,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
           ],
         ));
